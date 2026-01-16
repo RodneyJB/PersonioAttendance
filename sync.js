54c0d9102c2c8fe3edf10856b6da652b3276fa38
Rodney Buljubasic <rodney@bootepolch.local>
2026-01-16 08:58:15 +0100

    Fix missing function header for getAttendances

--- sync.js
+++ sync.js
@@ -28,6 +28,8 @@ async function getEmployee(employeeId) {
     return {};
   }
 }
+
+async function getAttendances() {
   const today = new Date().toISOString().slice(0, 10);
   const yesterday = new Date(Date.now() - 86400000).toISOString().slice(0, 10);
   const token = await getPersonioToken();
